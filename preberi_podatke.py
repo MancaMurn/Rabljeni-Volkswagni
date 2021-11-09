ee58849d7839a086cf70cb7d988788c78ba44f9d
Manca Murn <manci.murn10@gmail.com>
2021-11-09 19:23:44 +0100

    pobiranje podatkov

---
+++ preberi_podatke.py
@@ -0,0 +1,67 @@
+import re
+import orodja
+
+
+vzorec_bloka = re.compile(
+    r'<!-- SHOW TOP PONUDBA -*?>'
+    r'.*?'
+    r'<!-- SHOW BANNER -*?>',
+    flags=re.DOTALL
+)
+
+
+vzorec_avto = re.compile(
+    r'<!-- SHOW BANNER -+>.*?'
+    r'<div class="row bg-white .*?'
+    r'<span>(?P<ime>.+?)</span>.*?'
+    r'1.registracija.*?<td class=.*?>(?P<leto_prve_registracije>.+?)</td>.*?'
+    r'Prevoženih.*?<td class=.*?>(?P<prevozeni_kilometri>.+?) km</td>.*?'
+    r'Gorivo.*?<td class=.*?>(?P<vrsta_goriva>.+?)</td>.*?'
+    r'Menjalnik.*?<td class=.*?>\n *(?P<vrsta_menjalnika>.+?)\n *</td>.*?'
+    r'Motor.*?<td class=.*?>(?P<vrsta_motorja>.+?)</td>.*?'
+    r'<div class="GO-Results.*?Price-TXT-.*?">(?P<cena>.+?)€</div>',
+    flags=re.DOTALL
+)
+
+
+vzorec_model = re.compile(
+    r'[Vv]olkswagen (?P<model>.*?) .*',
+    flags=re.DOTALL
+)
+
+
+
+def izloci_podatke_avta(seznam_slovarjev):
+    for i in range(len(seznam_slovarjev)):
+        avto = seznam_slovarjev[i]
+        avto['leto_prve_registracije'] = int(avto['leto_prve_registracije'])
+        avto['prevozeni_kilometri'] = int(avto['prevozeni_kilometri'])
+        #avto['cena'] = int(avto['cena'])
+        avto['ime'] = vzorec_model.search(avto['ime']).group(1)
+
+# bloki = []
+# for i in range(1, 22):
+#     stran = orodja.vsebina_datoteke(f"stran_{i}.html")
+#     zadetki = vzorec_bloka.findall(stran)
+#     for zadetek in zadetki:
+#         bloki.append(zadetek)
+
+
+avti = []
+stevec = 0
+for i in range(1, 22):
+    stran = orodja.vsebina_datoteke(f"stran_{i}.html")
+    najdeni = vzorec_avto.finditer(stran)
+    for avto in najdeni:
+        # print(stevec, avto.groupdict())
+        stevec += 1
+        avti.append(avto.groupdict())
+
+izloci_podatke_avta(avti)
+print(avti)
+
+orodja.zapisi_json(avti, 'obdelani-podatki/avtomobili.json')
+orodja.zapisi_csv(
+    avti, 
+    ['ime', 'leto_prve_registracije', 'prevozeni_kilometri', 'vrsta_goriva', 'vrsta_menjalnika', 'vrsta_motorja', 'cena'],
+    'obdelani_podatki/avtomobili.csv')
